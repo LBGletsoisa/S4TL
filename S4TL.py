43303a652a57c347f0edc4272d0e7cf4bb640c4f
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-05-31 03:58:40 +0200

    Add files via upload

---
+++ S4TL.py
@@ -0,0 +1,10 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
+st.subheader("Sensor 4")
+
+st.write("Sensor 4 in the basement tunnel, close to access control has detected temperatures lower than the threshold of 35℃")
+
