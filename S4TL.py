19e63a8b16ab27e0fdbba38ba0a5736c629d45f7
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-06-01 23:53:21 +0200

    Update S4TL.py

--- S4TL.py
+++ S4TL.py
@@ -6,5 +6,5 @@ import streamlit as st
 st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
 st.subheader("Sensor 4")
 
-st.write("Sensor 4 in the basement tunnel, close to access control has detected temperatures lower than the threshold of 35℃")
+st.write("Sensor 4 in the basement tunnel, close to access control has detected temperatures lower than the threshold of 15℃")
 
